b333978feac22ca4fdddee1ffe73d24efc2ab37e
Quoc-Anh Vu <quoc.v.anh@gmail.com>
2022-05-02 13:29:07 -0700

    Check asdf plugin installation status before installing to save time

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ NC='\033[0m'
 function context() {
     echo "\n${ORANGE}${1}${NC}"
 }
+function message() {
+    echo "${ORANGE}${1}${NC}"
+}
 function run() {
     echo "${CYAN}${1}${NC}"
     eval $1
@@ -19,19 +22,6 @@ context 'Symlinking config files'
 run 'ln -s $HOME/.config/dotfiles/.config/* $HOME/.config/'
 run 'ln -s $HOME/.config/dotfiles/.tmux.conf $HOME/.config/dotfiles/.zshrc $HOME/.config/dotfiles/.profile $HOME/'
 
-# https://asdf-vm.com/guide/getting-started.html
-context 'Installing asdf, nodejs, python'
-run 'git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.9.0'
-run 'source ~/.zshrc'
-# https://github.com/danhper/asdf-python
-run 'asdf plugin-add python'
-run 'asdf install python latest'
-run 'asdf global python latest'
-# https://github.com/asdf-vm/asdf-nodejs
-run 'asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git'
-run 'asdf install nodejs latest'
-run 'asdf global nodejs latest'
-
 context 'Installing shell customizations: oh-my-zsh and powerlevel10k'
 # https://github.com/ohmyzsh/ohmyzsh#advanced-installation
 run 'git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.config/.oh-my-zsh'
@@ -43,6 +33,27 @@ run "sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'"
 
 run 'mkdir -p $HOME/ws/3p'
+
+# https://asdf-vm.com/guide/getting-started.html
+context 'Installing asdf, nodejs, python'
+run 'git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.9.0'
+run 'source ~/.zshrc'
+# https://github.com/danhper/asdf-python
+if ! asdf current python ; then
+    run 'asdf plugin-add python'
+    run 'asdf install python latest'
+    run 'asdf global python latest'
+else
+    message 'Skipping python - already installed.'
+fi
+# https://github.com/asdf-vm/asdf-nodejs
+if ! asdf current nodejs ; then
+    run 'asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git'
+    run 'asdf install nodejs latest'
+    run 'asdf global nodejs latest'
+else
+    message 'Skipping nodejs - already installed.' 
+fi
 
 context 'Installing font: UbuntuMono Nerd Font'
 # https://github.com/ryanoasis/nerd-fonts#option-6-ad-hoc-curl-download
