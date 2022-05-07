73a322688ab08989ae88e5e28ef0c176c1164a26
Quoc-Anh Vu <quoc.v.anh@gmail.com>
2022-05-06 18:48:14 -0700

    Added tmux, removed nodejs/python build dependencies (asdf pulls prebuilt binaries)

--- preinstall.sh
+++ preinstall.sh
@@ -2,14 +2,14 @@
 
 sudo dnf install -y curl git                # dotfiles dependency
 
-sudo dnf install -y zsh neovim ripgrep      # Shell + text editor
+sudo dnf install -y zsh tmux neovim ripgrep # Shell + text editor
 
 # python build dependencies (https://github.com/pyenv/pyenv/wiki#suggested-build-environment)
-sudo dnf install -y make gcc zlib-devel bzip2 bzip2-devel \
-    readline-devel sqlite sqlite-devel \
-    openssl-devel tk-devel libffi-devel xz-devel
-
-sudo dnf install -y gcc-c++                 # nodejs build dependencies (https://github.com/nodejs/node/blob/master/BUILDING.md#building-nodejs-on-supported-platforms)
+#sudo dnf install -y make gcc zlib-devel bzip2 bzip2-devel \
+#    readline-devel sqlite sqlite-devel \
+#    openssl-devel tk-devel libffi-devel xz-devel
+#
+#sudo dnf install -y gcc-c++                 # nodejs build dependencies (https://github.com/nodejs/node/blob/master/BUILDING.md#building-nodejs-on-supported-platforms)
 
 #sudo dnf install -y i3 polybar sway waybar  # window managers: i3/sway + polybar/waybar
 
