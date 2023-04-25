ab10261db5f0e3d1d4e03e51dec5099d4b77fe16
Quoc-Anh Vu <quoc.v.anh@gmail.com>
2023-04-25 12:02:33 -0700

    Added Nord color theme to alacritty

--- install.sh
+++ install.sh
@@ -36,6 +36,10 @@ run 'git clone https://github.com/ohmyzsh/ohmyzsh.git $XDG_DATA_HOME/oh-my-zsh'
 run 'git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $XDG_DATA_HOME/oh-my-zsh/custom/themes/powerlevel10k'
 
 context 'Installing vim customizations: vim-plug'
+message 'Run <prefix>-I in tmux to install plugins!'
+run "git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm"
+
+context 'Installing tmux customizations: tpm'
 run "sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'"
 
