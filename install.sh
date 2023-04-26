d888152dbf9fc07b1de7e9f8afe07df8f9852ba3
Quoc-Anh Vu <quoc.v.anh@gmail.com>
2023-04-25 17:50:52 -0700

    Added visibility into preinstall.sh + upgraded visibility commands

--- install.sh
+++ install.sh
@@ -4,37 +4,36 @@ ORANGE='\033[0;33m'
 CYAN='\033[0;36m'
 NC='\033[0m'
 function context() {
-    echo "\n${ORANGE}${1}${NC}"
+    echo "\n${ORANGE}${@:1}${NC}"
 }
 function message() {
-    echo "${ORANGE}${1}${NC}"
+    echo "${ORANGE}${@:1}${NC}"
 }
 function run() {
-    echo "${CYAN}${1}${NC}"
-    eval $1
+    echo "${CYAN}${@:1}${NC}"
+    eval ${@:1}
 }
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_STATE_HOME="$HOME/.local/state"
-
-run 'mkdir -p $XDG_CONFIG_HOME'
-run 'mkdir -p $XDG_CACHE_HOME'
-run 'mkdir -p $XDG_DATA_HOME'
-run 'mkdir -p $XDG_STATE_HOME'
+run mkdir -p $XDG_CONFIG_HOME
+run mkdir -p $XDG_CACHE_HOME
+run mkdir -p $XDG_DATA_HOME
+run mkdir -p $XDG_STATE_HOME
 
 context 'Installing dotfiles'
-run 'mkdir -p $HOME/ws'
-run 'git clone https://github.com/QuocAnhVu/dotfiles.git $HOME/ws/dotfiles'
+run mkdir -p $HOME/ws
+run git clone https://github.com/QuocAnhVu/dotfiles.git $HOME/ws/dotfiles
 context 'Symlinking config files'
-run 'ln -s $HOME/ws/dotfiles/.config/* $HOME/.config/'
-run 'ln -s $HOME/ws/dotfiles/.zshrc $HOME/'
+run ln -s $HOME/ws/dotfiles/.config/* $HOME/.config/
+run ln -s $HOME/ws/dotfiles/.zshrc $HOME/
 
 context 'Installing shell customizations: oh-my-zsh and powerlevel10k'
 # https://github.com/ohmyzsh/ohmyzsh#advanced-installation
-run 'git clone https://github.com/ohmyzsh/ohmyzsh.git $XDG_DATA_HOME/oh-my-zsh'
+run git clone https://github.com/ohmyzsh/ohmyzsh.git $XDG_DATA_HOME/oh-my-zsh
 # https://github.com/romkatv/powerlevel10k#oh-my-zsh
-run 'git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $XDG_DATA_HOME/oh-my-zsh/custom/themes/powerlevel10k'
+run git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $XDG_DATA_HOME/oh-my-zsh/custom/themes/powerlevel10k
 
 context 'Installing vim customizations: vim-plug'
 message 'Run <prefix>-I in tmux to install plugins!'
@@ -46,21 +45,21 @@ run "sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/
 
 # https://asdf-vm.com/guide/getting-started.html
 context 'Installing asdf, nodejs, python'
-run 'git clone https://github.com/asdf-vm/asdf.git ~/.local/share/asdf --branch v0.11.3'
-run 'source ~/.zshrc'
+run git clone https://github.com/asdf-vm/asdf.git ~/.local/share/asdf --branch v0.11.3
+run source ~/.zshrc
 # https://github.com/danhper/asdf-python
 if ! asdf current python ; then
-    run 'asdf plugin-add python'
-    run 'asdf install python latest'
-    run 'asdf global python latest'
+    run asdf plugin-add python
+    run asdf install python latest
+    run asdf global python latest
 else
     message 'asdf python detected - no installation needed.'
 fi
 # https://github.com/asdf-vm/asdf-nodejs
 if ! asdf current nodejs ; then
-    run 'asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git'
-    run 'asdf install nodejs lts'
-    run 'asdf global nodejs lts'
+    run asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+    run asdf install nodejs lts
+    run asdf global nodejs lts
 else
     message 'asdf nodejs detected - no installation needed.' 
 fi
@@ -68,13 +67,13 @@ fi
 context 'Installing font: UbuntuMono Nerd Font'
 if ! fc-list | grep 'UbuntuMono Nerd Font' > /dev/null ; then
     # https://github.com/ryanoasis/nerd-fonts#option-6-ad-hoc-curl-download
-    run 'git clone --filter=blob:none --sparse https://github.com/ryanoasis/nerd-fonts.git $XDG_CACHE_HOME/nerd-fonts'
+    run git clone --filter=blob:none --sparse https://github.com/ryanoasis/nerd-fonts.git $XDG_CACHE_HOME/nerd-fonts
     # https://github.com/ryanoasis/nerd-fonts#option-3-install-script
-    run 'pushd $XDG_CACHE_HOME/nerd-fonts'
-    run 'git sparse-checkout add patched-fonts/UbuntuMono'
-    run './install.sh UbuntuMono'
-    run 'popd'
-    run 'rm -rf $XDG_CACHE_HOME/nerd-fonts'
+    run pushd $XDG_CACHE_HOME/nerd-fonts
+    run git sparse-checkout add patched-fonts/UbuntuMono
+    run ./install.sh UbuntuMono
+    run popd
+    run rm -rf $XDG_CACHE_HOME/nerd-fonts
 else
     message 'UbuntuMono Nerd Font detected - no installation needed.'
 fi
--- preinstall.sh
+++ preinstall.sh
@@ -1,58 +1,83 @@
 #! /usr/bin/bash
+ORANGE='\033[0;33m'
+CYAN='\033[0;36m'
+NC='\033[0m'
+function context() {
+    echo "\n${ORANGE}${@:1}${NC}"
+}
+function message() {
+    echo "${ORANGE}${@:1}${NC}"
+}
+function run() {
+    echo "${CYAN}${@:1}${NC}"
+    eval ${@:1}
+}
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_STATE_HOME="$HOME/.local/state"
+run mkdir -p $XDG_CONFIG_HOME
+run mkdir -p $XDG_CACHE_HOME
+run mkdir -p $XDG_DATA_HOME
+run mkdir -p $XDG_STATE_HOME
 
 if command -v apt &> /dev/null; then
     PKG=apt
 elif command -v dnf &> /dev/null; then
     PKG=dnf
 else
-    echo "Distro's package manager is not supported."
+    message "Distro's package manager is not supported."
     exit
 fi
 
-sudo $PKG update
-sudo $PKG install -y curl git zsh tmux neovim ripgrep
-/usr/bin/pip3 install neovim
+context 'Installing packages'
+run sudo $PKG update
+run sudo $PKG install -y curl git zsh tmux neovim ripgrep
+run /usr/bin/pip3 install neovim
 
 # python build dependencies (https://github.com/pyenv/pyenv/wiki#suggested-build-environment)
+context 'Installing python build dependencies'
 if command -v apt &> /dev/null; then
-    sudo apt install -y make build-essential libssl-dev zlib1g-dev \
+    run sudo apt install -y make build-essential libssl-dev zlib1g-dev \
         libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
         libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 elif command -v dnf &> /dev/null; then
-    sudo dnf install -y make gcc zlib-devel bzip2 bzip2-devel \
+    run sudo dnf install -y make gcc zlib-devel bzip2 bzip2-devel \
         readline-devel sqlite sqlite-devel \
         openssl-devel tk-devel libffi-devel xz-devel
 else
-    echo "Distro's package manager is not supported."
-    exit
+    message "Installing python build dependencies failed"
 fi
 
 # nodejs build dependencies (https://github.com/nodejs/node/blob/master/BUILDING.md#building-nodejs-on-supported-platforms)
+context 'Installing nodejs build dependencies'
 if command -v apt &> /dev/null; then
-    sudo apt install -y python3 g++ make python3-pip
+    run sudo apt install -y python3 g++ make python3-pip
 elif command -v dnf &> /dev/null; then
-    sudo dnf install -y python3 gcc-c++ make python3-pip
+    run sudo dnf install -y python3 gcc-c++ make python3-pip
 else
-    echo "Distro's package manager is not supported."
-    exit
+    message "Installing nodejs build dependencies failed"
 fi
 
 # window managers: i3/sway + polybar/waybar
 #sudo $PKG install -y i3 polybar sway waybar
 
 # Set default shell to ZSH
+context 'Changing default shell to zsh'
 if command -v chsh &> /dev/null; then
-    sudo chsh -s $(which zsh)
+    run sudo chsh -s $(which zsh)
 elif command -v lchsh &> /dev/null; then
-    sudo lchsh $USER  # set to /usr/bin/zsh
+    run sudo lchsh $USER  # set to /usr/bin/zsh
 else
-    echo "Warning: default shell could not be changed to zsh."
+    message "Warning: default shell could not be changed to zsh."
 fi
 
 # Ubuntu specific
 if command -v chsh &> /dev/null; then
-    sudo apt install -y fontconfig
+    context ' Installing Ubuntu specific packages'
+    run sudo apt install -y fontconfig
 fi
 
 # Remove old config files
-rm -rf ~/.zshrc ~/.tmux.conf ~/.config/tmux ~/.profile ~/.p10k.zsh ~/.config/alacritty ~/.config/i3 ~/.config/nvim ~/.config/polybar ~/.config/powerlevel10k ~/.config/sway ~/.config/waybar ~/.asdf ~/.config/dotfiles
+context 'Removing old config files'
+run rm -rf ~/.zshrc ~/.tmux.conf ~/.config/tmux ~/.profile ~/.p10k.zsh ~/.config/alacritty ~/.config/i3 ~/.config/nvim ~/.config/polybar ~/.config/powerlevel10k ~/.config/sway ~/.config/waybar ~/.asdf ~/.config/dotfiles
