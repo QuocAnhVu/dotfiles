62de9c3f834e7e14b470485b94bd653252ac11af
Quoc-Anh Vu <quoc.v.anh@gmail.com>
2023-04-26 00:55:55 -0700

    [ssh] Improved ssh script by avoiding overwrites, redundant writes, and visibility message

--- harden_ssh.sh
+++ harden_ssh.sh
@@ -1,56 +1,97 @@
 #! /usr/bin/zsh
 # https://stribika.github.io/2015/01/04/secure-secure-shell.html
 
-# Run this first
-DOTFILES=$HOME/.config/dotfiles
-mkdir -p $HOME/.ssh
-chmod 700 $HOME/.ssh
+ORANGE='\033[0;33m'
+CYAN='\033[0;36m'
+NC='\033[0m'
+function context() {
+    echo "\n${ORANGE}${@:1}${NC}"
+}
+function message() {
+    echo "${ORANGE}${@:1}${NC}"
+}
+function run() {
+    echo "${CYAN}${@:1}${NC}"
+    eval ${@:1}
+}
+XDG_CONFIG_HOME="$HOME/.config"
+XDG_CACHE_HOME="$HOME/.cache"
+XDG_DATA_HOME="$HOME/.local/share"
+XDG_STATE_HOME="$HOME/.local/state"
 
-# Install sshd
+context 'Setting up dotfiles directory'
+run mkdir -p $HOME/.ssh
+run chmod 700 $HOME/.ssh
+
+context 'Installing sshd and mosh'
 if command -v apt &> /dev/null; then
-    sudo apt update; sudo apt -y install openssh-server mosh
+    run sudo apt update; run sudo apt -y install openssh-server mosh
 elif command -v dnf &> /dev/null; then
-    sudo dnf install -y openssh-server mosh
+    run sudo dnf install -y openssh-server mosh
 else
-    echo "Distro's package manager is not supported."
+    message "Distro's package manager is not supported."
     exit
 fi
 
-# Allow access from BAWK
-cat $DOTFILES/.ssh/authorized_keys >> $HOME/.ssh/authorized_keys
-chmod 644 $HOME/.ssh/authorized_keys
+context 'Allowing access from BAWK'
+if [ ! -f $HOME/.ssh/authorized_keys ]; then
+    run touch $HOME/.ssh/authorized_keys
+    run chmod 644 $HOME/.ssh/authorized_keys
+fi
+while read key; do
+    if ! grep -qF "$key" $HOME/.ssh/authorized_keys; then
+        run echo "$key" >> $HOME/.ssh/authorized_keys
+    fi
+done < $DOTFILES/.ssh/authorized_keys
+context 'Restricting SSH access to ssh-user group'
+run sudo groupadd ssh-user
+run sudo usermod -a -G ssh-user $USER
 
-# SSH access is restricted to only the ssh-user group
-sudo groupadd ssh-user
-sudo usermod -a -G ssh-user $USER
+context 'Configuring ssh with hardened config'
+if [ -f /etc/ssh/ssh_config ]; then
+    run sudo mv /etc/ssh/ssh_config /etc/ssh/ssh_config.orig
+fi
+run sudo cp $DOTFILES/.ssh/ssh_config.default /etc/ssh/ssh_config
 
-# Configure ssh with hardened config
-sudo mv /etc/ssh/ssh_config /etc/ssh/ssh_config.orig
-sudo cp $DOTFILES/.ssh/ssh_config.default /etc/ssh/ssh_config
+context 'Configuring sshd with hardened config'
+if [ -f /etc/ssh/sshd_config ]; then
+    run sudo mv /etc/ssh/sshd_config /etc/ssh/sshd_config.orig
+fi
+run sudo cp $DOTFILES/.ssh/sshd_config.default /etc/ssh/sshd_config
 
-# Configure sshd with hardened config
-# TODO: Use tor hidden services
-sudo mv /etc/ssh/sshd_config /etc/ssh/sshd_config.orig
-sudo cp $DOTFILES/.ssh/sshd_config.default /etc/ssh/sshd_config
+context 'Removing weak prime numbers/generators'
+run awk '$5 > 2000' /etc/ssh/moduli > "${HOME}/moduli"
+run wc -l "${HOME}/moduli" # make sure there is something left
+run sudo mv "${HOME}/moduli" /etc/ssh/moduli
+run rm $HOME/moduli
 
-# Remove weak prime numbers/generators
-# Unnecessary in at least Fedora 35+
-awk '$5 > 2000' /etc/ssh/moduli > "${HOME}/moduli"
-wc -l "${HOME}/moduli" # make sure there is something left
-sudo mv "${HOME}/moduli" /etc/ssh/moduli
-rm $HOME/moduli
-
-# Harden host keys
+context 'Hardening host keys'
 pushd /etc/ssh
-sudo rm ssh_host_*key*
-sudo ssh-keygen -t ed25519 -f ssh_host_ed25519_key -N "" < /dev/null
-sudo ssh-keygen -t rsa -b 4096 -f ssh_host_rsa_key -N "" < /dev/null
+run sudo rm ssh_host_*key*
+if [ ! -f /etc/ssh/ssh_host_ed25519_key ]; then
+    run sudo ssh-keygen -t ed25519 -f ssh_host_ed25519_key -N "" < /dev/null
+else
+    message 'Skipping ssh_host_ed25519_key generation'
+fi
+if [ ! -f /etc/ssh/ssh_host_rsa_key ] || [ $(ssh-keygen -lf /etc/ssh/ssh_host_rsa_key | awk '{print $1}') -ne 4096 ]; then
+    run sudo ssh-keygen -t rsa -b 4096 -f ssh_host_rsa_key -N "" < /dev/null
+else
+    message 'Skipping ssh_host_rsa_key generation'
+fi
 popd
 
-# Generate client keys
-ssh-keygen -t ed25519 -o -a 100
-ssh-keygen -t rsa -b 4096 -o -a 100
+context 'Generating client keys'
+if [ ! -f $HOME/.ssh/id_ed25519 ]; then
+    run ssh-keygen -t ed25519 -o -a 100 -f $HOME/.ssh/id_ed25519
+else
+    message 'Skipping id_ed25519 key generation'
+fi
+if [ ! -f $HOME/.ssh/id_rsa ] || [ $(ssh-keygen -lf $HOME/.ssh/id_rsa | awk '{print $1}') -ne 4096 ]; then
+    run ssh-keygen -t rsa -b 4096 -o -a 100 -f $HOME/.ssh/id_rsa
+else
+    message 'Skipping id_rsa key generation'
+fi
 
-# Start sshd
-sudo systemctl enable sshd
-sudo systemctl start sshd
+context 'Starting sshd service'
+run sudo systemctl enable sshd
+run sudo systemctl start sshd
