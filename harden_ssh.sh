408c4b233ee68621f66f2db2cffda05b1c2c7bad
Quoc-Anh Vu <quoc.v.anh@gmail.com>
2023-09-22 10:22:07 -0700

    Update dotfile directory in harden_ssh.sh

--- harden_ssh.sh
+++ harden_ssh.sh
@@ -24,7 +24,7 @@ XDG_CONFIG_HOME="$HOME/.config"
 XDG_CACHE_HOME="$HOME/.cache"
 XDG_DATA_HOME="$HOME/.local/share"
 XDG_STATE_HOME="$HOME/.local/state"
-DOTFILES="$HOME/ws/dotfiles"
+DOTFILES="$XDG_DATA_HOME/dotfiles"
 
 context 'Setting up dotfiles directory'
 run mkdir -p $HOME/.ssh
