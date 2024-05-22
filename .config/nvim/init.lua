e6f34e35bb83dd26f083251eb15e968126e80cf3
Quoc-Anh Vu <quoc.v.anh@gmail.com>
2024-05-21 23:54:04 -0700

    [nvim] Change prefix from 'f' to '<leader>' for Telescope

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -315,10 +315,10 @@ require('lazy').setup({
 			pcall(require('telescope').load_extension, 'ui-select')
 
 			local builtin = require('telescope.builtin')
-			vim.keymap.set('n', 'ff', builtin.find_files, {})
-			vim.keymap.set('n', 'fg', builtin.live_grep, {})
-			vim.keymap.set('n', 'fb', builtin.buffers, {})
-			vim.keymap.set('n', 'fh', builtin.help_tags, {})
+			vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+			vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+			vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+			vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
 		end,
 	},
 	{
